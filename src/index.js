6b5bf78fcf454d88e320b7026c26e3f8859da6a8
Pavel Cherepanov <pavel@cherepanov.pro>
2018-05-28 22:54:49 +0300

    Eslint is connected to the project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const identifyUser = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+const identifyUser = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
+
+export default identifyUser;

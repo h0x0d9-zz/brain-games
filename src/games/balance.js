4cb231f177fc7e7be661569352d6a55df6c13ae0
Pavel Cherepanov <pavel@cherepanov.pro>
2018-05-30 16:13:48 +0300

    fix (balance) change recursive method balanceNumber on iteration

--- src/games/balance.js
+++ src/games/balance.js
@@ -8,23 +8,15 @@ const valueMax = 1000;
 
 const balanceNumber = (num) => {
   const digitizedNumber = String(num).split('').map(Number);
+  let sortedNumbers = digitizedNumber.sort();
 
-  const iter = (digits) => {
-    const acc = digits.sort();
-    const min = digits[0];
-    const max = digits[digits.length - 1];
+  while (sortedNumbers[sortedNumbers.length - 1] - sortedNumbers[0] > 1) {
+    sortedNumbers[sortedNumbers.length - 1] -= 1;
+    sortedNumbers[0] += 1;
+    sortedNumbers = sortedNumbers.sort();
+  }
 
-    if (max - min > 1) {
-      acc[digits.length - 1] -= 1;
-      acc[0] += 1;
-
-      return iter(acc);
-    }
-
-    return acc.join('');
-  };
-
-  return Number(iter(digitizedNumber));
+  return Number(sortedNumbers.join(''));
 };
 
 const askBalance = () => {

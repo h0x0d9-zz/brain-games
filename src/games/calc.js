0b94e9f2a3898faf298b40ff9118447c3e6f4949
Pavel Cherepanov <pavel@cherepanov.pro>
2018-05-30 12:28:47 +0300

    chore (calc) rename function which generate game data

--- src/games/calc.js
+++ src/games/calc.js
@@ -6,7 +6,7 @@ const rule = 'What is the result of the expression?\n';
 const valueMin = 1;
 const valueMax = 100;
 
-const generateExpression = (num1, num2) => {
+const generateGameData = (num1, num2) => {
   switch (generateNumber(1, 3)) {
     case 1:
       return cons(`${num1} + ${num2}`, `${num1 + num2}`);
@@ -20,7 +20,7 @@ const generateExpression = (num1, num2) => {
 const askCalc = () => {
   const num1 = generateNumber(valueMin, valueMax);
   const num2 = generateNumber(valueMin, valueMax);
-  return generateExpression(num1, num2);
+  return generateGameData(num1, num2);
 };
 
 export default () => makeGame(askCalc, rule);

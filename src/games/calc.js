b3ec9fa467e9f29240282d976fb10ddd60414a26
Pavel Cherepanov <pavel@cherepanov.pro>
2018-05-31 17:04:24 +0300

    chore (calc) rename askCal to makeGameData and transfer generateData -> makeGameData scope

--- src/games/calc.js
+++ src/games/calc.js
@@ -6,21 +6,21 @@ const rule = 'What is the result of the expression?';
 const valueMin = 1;
 const valueMax = 100;
 
-const generateGameData = (num1, num2) => {
-  switch (generateNumber(1, 3)) {
-    case 1:
-      return cons(`${num1} + ${num2}`, `${num1 + num2}`);
-    case 2:
-      return cons(`${num1} - ${num2}`, `${num1 - num2}`);
-    default:
-      return cons(`${num1} * ${num2}`, `${num1 * num2}`);
-  }
-};
+const makeGameData = () => {
+  const generateData = (num1, num2) => {
+    switch (generateNumber(1, 3)) {
+      case 1:
+        return cons(`${num1} + ${num2}`, `${num1 + num2}`);
+      case 2:
+        return cons(`${num1} - ${num2}`, `${num1 - num2}`);
+      default:
+        return cons(`${num1} * ${num2}`, `${num1 * num2}`);
+    }
+  };
 
-const askCalc = () => {
   const num1 = generateNumber(valueMin, valueMax);
   const num2 = generateNumber(valueMin, valueMax);
-  return generateGameData(num1, num2);
+  return generateData(num1, num2);
 };
 
-export default () => makeGame(askCalc, rule);
+export default () => makeGame(makeGameData, rule);
